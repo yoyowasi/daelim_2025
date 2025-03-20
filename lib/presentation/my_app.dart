18ed2eeca31f8cb5645fa2c3a9717f7975c28bcc
yoyowasi <youmin302@gmail.com>
2025-03-20 12:42:26 +0900

    MyApp 분리,홈화면 생성성

---
+++ lib/presentation/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:daelim_2025/presentation/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: HomeScreen()
+    );
+  }
+}
