10a5fe21759fc5d86ab61e2997a3eb12a09f640c
yoyowasi <youmin302@gmail.com>
2025-03-27 13:25:38 +0900

    03월27일 수업 내용

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _count = 0;
+
+  void _handleIncreaseCount() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 11, 178),
+        title: Text('Flutter Demo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _handleIncreaseCount();
+        },
+        child: Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('버튼 클릭시 횟수 증가', style: TextStyle(fontSize: 34)),
+              Text('$_count', style: TextStyle(fontSize: 40)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
